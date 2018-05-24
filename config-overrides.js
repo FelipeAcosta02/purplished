3ba07b966d09ddd2e88912b3476a4c7292258a0b
FelipeAcosta02 <jesusfacosta02@gmail.com>
2018-05-24 09:09:46 -0600

    added modules

--- config-overrides.js
+++ config-overrides.js
@@ -6,7 +6,7 @@ const rewireCssModules = require('react-app-rewire-css-modules');
 module.exports = function override(config, env) {
 
     config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
-    config = rewireLess.withLoaderOptions({modifyVars: { "@primary-color": "#1DA57A" },})(config, env);
+    config = rewireLess.withLoaderOptions({modifyVars: { "@primary-color": "#722ED1" },})(config, env);
     config = rewireCssModules(config, env);  
 
   return config;
