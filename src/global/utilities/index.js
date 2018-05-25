ca383147d68d652e35c4c21236b671b7a3d65c49
FelipeAcosta02 <jesusfacosta02@gmail.com>
2018-05-25 11:23:14 -0600

    fixed utils and added indexes to /global

--- src/global/utilities/index.js
+++ src/global/utilities/index.js
@@ -1,4 +1,16 @@
-//General-purpose functions
+//The place for General-purpose functions
+// feel free to add functions that seem short or are single-line directly into this file.
+// also feel free to add big fat complex functions into their own file
+//happy hacking
 
-//Create a random ID that is always unique
+// All exports from other files go here:
+
+
+
+// all exports from this file start here (be sure to comment what they do) PS: have a happy hacking!
+
+//Creates a random ID that is always unique
 export const generateID = () => '_' + Math.random().toString(36).substr(2,9);
+
+//Converts Normal Strings to kebab-case
+export const convert = (string) => string.split(" ").join("-").toLowerCase();
