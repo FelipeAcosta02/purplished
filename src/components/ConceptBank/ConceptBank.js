aab888881aa846166629fd396b0ab534acab52bd
FelipeAcosta02 <jesusfacosta02@gmail.com>
2018-05-25 11:12:29 -0600

    ConceptBank start

--- src/components/ConceptBank/ConceptBank.js
+++ src/components/ConceptBank/ConceptBank.js
@@ -1,56 +1,5 @@
-import React from 'react';
-import styles from './ConceptBank.module.scss';
-import QuestionContainer from './QuestionContainer/QuestionContainer'
-import { generateID } from 'global/utilities'
+import React from 'react'
 
-import { Card, Row, Col, Icon } from 'antd';
-const Content = Card.Grid;
+export default class ConceptBank extends React.Component {
 
-class ConceptBank extends React.Component {
-    state = {
-        questionObjectsArray: [],
-        questionContainers: []
-    }
-
-    addQuestionHandler = (event) => {
-        const newQuestionContainers = [...this.state.questionContainers]
-        const id = generateID()
-        newQuestionContainers.push(<QuestionContainer id={id} delete={this.deleteQuestionHandler}/>)
-        this.setState({questionContainers: newQuestionContainers})
-    }
-
-    deleteQuestionHandler = (index) => {
-                console.log(index)
-                const newQuestionContainers = [...this.state.questionContainers]
-                newQuestionContainers.splice(index, 1)                                  // I was just about to change splice() to filter()
-                this.setState({questionContainers: newQuestionContainers})              // Please fullfill my destiny
-                console.log(this.state.questionContainers, index)  
-    }
-    
-    render() {
-        let content = this.state.questionContainers.map((question, index) => {
-            return(
-                <Content key={index} className={styles.grid}>
-                 {question}
-                </Content>
-            )
-
-          }
-        )
-        return(
-            <Row type={'flex'} justify="center" className={styles.row}>
-                <Col span={22}>
-                    <Card 
-                    bordered
-                    actions={[<Icon type="plus-circle-o" 
-                            onClick={this.addQuestionHandler}/>]}>
-                            <h1>{this.props.name}</h1>
-                            {content}
-                    </Card> 
-                </Col>
-            </Row>
-        )
-    }
 }
-
-export default ConceptBank;
