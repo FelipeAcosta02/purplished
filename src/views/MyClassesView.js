ad6090a24fc51b6dd97c8f2174c42a3d62f83c35
FelipeAcosta02 <jesusfacosta02@gmail.com>
2018-05-30 13:20:52 -0600

    one last

--- src/views/MyClassesView.js
+++ src/views/MyClassesView.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import ConceptBank from 'components/ConceptBank/ConceptBank'
-
 
 class MyClassesView extends React.Component {
     render() {
         return(
             <div>
-                <ConceptBank/>
             </div>
         )
     }
